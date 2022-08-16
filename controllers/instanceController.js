32e61c2280852c080919b8df949faf60cdec9cbb
razaqisama <107979029+razaqi@users.noreply.github.com>
2022-08-16 15:00:40 +0700

    check instance send message

--- controllers/instanceController.js
+++ controllers/instanceController.js
@@ -1,6 +1,8 @@
 const database = require("../models")
 const registeredChannels = require("./channels.json")
 const { validateFormat } = require('../helpers')
+const axios = require('axios');
+
 
 const saveMessageToDatabase = async (req, res, next) => {
 	try {
@@ -64,6 +66,109 @@ const saveMessageToDatabase = async (req, res, next) => {
 	}
 }
 
+const checkInstance = async (req, res, next) => {
+	try {
+		const {
+			sub_id,
+			instance_id,
+			recipient_id,
+			message,
+			room,
+			selectedRoom,
+			files,
+			replyMessage
+		} = req.body;
+
+
+		const settingsDb = await require("../models/settings.model")(
+			database.sequelize,
+			database.Sequelize,
+			`${sub_id}_settings`
+		);
+		
+		const instanceData = await settingsDb.findOne({
+			where: {
+				key: "instances"
+			}
+		})
+
+		const instance = instanceData?.dataValues?.value;
+
+
+		// ++++++ TINGGAL DI UNCOMENT DAN DISESUAIKAN 
+
+		// if(instance.phone_number_id) {
+		// 	const endpoint = process.env.WA_CLOUD_ENDPOINT
+		// 	const chatPayload = {
+		// 		content: message,
+		// 		sub_id,
+		// 		instance_id,
+		// 		room_id: room,
+		// 		recipient_number: recipient_id
+		// 	}
+
+		// 	const response = await axios.post(endpoint, chatPayload);
+			
+		// 	console.log(response.data);
+		// } else if (instance.username) {
+		// 	const endpoint = process.env.TWITTER_ENDPOINT
+		// 	const chatPayload = {
+		// 		sub_id,
+		// 		instance_id,
+		// 		room_id: selectedRoom,
+		// 		content: {
+		// 			text: message
+		// 		}
+		// 	}
+		// 	const response = await axios.post(endpoint, chatPayload)
+
+		// 	console.log(response.data);
+
+		// } else if (instance.id_page) {
+		// 	const endpoint = process.env.FACEBOOK_ENDPOINT
+		// 	const chatPayload = {
+		// 		to: recipient_id,
+		// 		platform: 'facebook',
+		// 		message,
+		// 		pageAccessToken: instance.pageAccessToken
+		// 	}
+		// 	const messageParams = {
+		// 		subId: sub_id,
+		// 		instanceId: instance_id
+		// 	}
+
+		// 	const response = await axios.post(endpoint, chatPayload, {
+		// 		params: messageParams
+		// 	})
+
+		// 	console.log(response.data);
+
+		// 	//operation to facebook account
+		// } else if (instance.id_instagram) {
+		// 	const endpoint = process.env.INSTAGRAM_ENDPOINT
+		// 	const chatPayload = {
+		// 		to: recipient_id,
+		// 		platform: 'instagram',
+		// 		message,
+		// 		pageAccessToken: instance.pageAccessToken
+		// 	}
+		// 	//operation to instagram account
+		// }
+		// ++++++ TINGGAL DI UNCOMENT DAN DISESUAIKAN 
+
+		res.status(200).json({
+			message: "OKEI"
+		})
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "ADA ERROR"
+		})
+	} 
+}
+
+
 module.exports = {
-	saveMessageToDatabase
+	saveMessageToDatabase,
+	checkInstance
 }
